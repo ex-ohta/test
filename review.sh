3851ce7c8be43922e2acee0054dc9353807a9a03
Gecko <cellpromote-ohta@co.kayac.com>
2017-05-17 15:38:06 +0900

    remove ruby update patch

--- review.sh
+++ review.sh
@@ -5,9 +5,6 @@ export LANG=C.UTF-8
 echo "********************"
 echo "* install gems     *"
 echo "********************"
-sudo add-apt-repository -y ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get -y install ruby2.1
 sudo gem install checkstyle_filter-git saddler saddler-reporter-github findbugs_translate_checkstyle_format android_lint_translate_checkstyle_format pmd_translate_checkstyle_format
 if [ $? -ne 0 ]; then
       echo 'Failed to install gems.'
