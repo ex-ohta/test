d1344ad071f3fb7a18a6c7679a786dffc8cb7ed8
Gecko <cellpromote-ohta@co.kayac.com>
2017-05-22 10:30:22 +0900

    modify review shell

--- review.sh
+++ review.sh
@@ -15,7 +15,7 @@ echo "********************"
 echo "* checkstyle       *"
 echo "********************"
 cat ./app/build/reports/checkstyle/checkstyle.xml \
-    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py \
+    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py -b origin/master\
     | /opt/python_tools/github_report/reporter.py 
 
 
@@ -24,7 +24,7 @@ echo "* findbugs         *"
 echo "********************"
 cat ./app/build/reports/findbugs/findbugs_report.xml \
     | findbugs_translate_checkstyle_format translate \
-    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py \
+    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py -b origin/master\
     | /opt/python_tools/github_report/reporter.py
 
 echo "********************"
@@ -32,7 +32,7 @@ echo "* PMD              *"
 echo "********************"
 cat ./app/build/reports/pmd/pmd.xml \
     | pmd_translate_checkstyle_format translate \
-    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py \
+    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py -b origin/master\
     | /opt/python_tools/github_report/reporter.py
 
 echo "********************"
@@ -40,7 +40,7 @@ echo "* PMD-CPD          *"
 echo "********************"
 cat ./app/build/reports/pmd/cpd.xml \
     | pmd_translate_checkstyle_format translate --cpd-translate \
-    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py \
+    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py -b origin/master\
     | /opt/python_tools/github_report/reporter.py
 
 echo "********************"
@@ -48,5 +48,5 @@ echo "* android lint     *"
 echo "********************"
 cat ./app/build/reports/lint/lint_results.xml \
     | android_lint_translate_checkstyle_format translate \
-    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py \
+    | /opt/python_tools/checkstyle_filter/checkstyle_filter_git_diff.py -b origin/master\
     | /opt/python_tools/github_report/reporter.py
